4196b5349e8665fc462403e3733404d92228c345
AmmarDagha <ammardagha@gmail.com>
2021-06-29 12:03:40 +0500

    Fixed errors

--- guessinggame.py
+++ guessinggame.py
@@ -6,20 +6,21 @@ numofguesses = 0
 
 while number != guess:
     print ("Sorry, try again!")
+    guess = int(input("Enter an integer from 0 to 10: "))
     numofguesses = numofguesses + 1
-    if numofguesses = 5:
-        hint = input(print ("Do you want a hint? Type y or n: "))
-        if hint = 'y':
+    if numofguesses == 4:
+        hint = int(input(print ("Do you want a hint? Type y or n: "))
+        if hint == "y":
             if number>5:
                 print("The number is greater than 5")
             elif number<5:
                 print("The number is less than 5")
-        if hint = 'n':
+        elif hint == "n":
             guess = int(input("Enter an integer from 0 to 10: "))
-    if numofguesses = 8:
+    if numofguesses == 7:
         print("You have failed to guess the number")
         print("The number was : ", number)
 
-if number = guess :
-    print("Congratulations! You guessed the number in ", numofguesses "number of tries")
+if number == guess :
+    print("Congratulations! You guessed the number in ", numofguesses, "number of tries")
         
